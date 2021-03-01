f14509e0fdbfda0ef4811e22a3cc8c11d13deb73
Mike Leonard <mtleonardjr@gmail.com>
2021-03-01 12:25:34 -0500

    adjusted sidebar placement

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <div className='header'>Header</div>
-      <div className='main-container'>
-        <div className='main-body'>
-          <div className='sidebar'>sidebar</div>
+    <div className='app-container'>
+      <div className='sidebar'>sidebar</div>
+      <div className="main-container">
+        <div className='header'>Header</div>
+        <div className='body-container'>
           <div className='grid'>
             <div className='grid-item-1'>grid-item-1</div>
             <div className='grid-item-2'>grid-item-2</div>
@@ -15,10 +15,9 @@ function App() {
             <div className='grid-item-5'>grid-item-5</div>
             <div className='grid-item-6'>grid-item-6</div>
           </div>
+          <div className='footer'>Footer</div>
         </div>
-        <div className='footer'>Footer</div>
       </div>
-
     </div>
   );
 }
