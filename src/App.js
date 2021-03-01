c263a118f1d206d4e26908056c6ab319542daaa3
Mike Leonard <mtleonardjr@gmail.com>
2021-03-01 12:04:23 -0500

    finished desktop layout

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ function App() {
         <div className='main-body'>
           <div className='sidebar'>sidebar</div>
           <div className='grid'>
-            <div className='grid-item'>grid-item</div>
-            <div className='grid-item'>grid-item</div>
-            <div className='grid-item'>grid-item</div>
-            <div className='grid-item'>grid-item</div>
-            <div className='grid-item'>grid-item</div>
-            <div className='grid-item'>grid-item</div>
+            <div className='grid-item-1'>grid-item-1</div>
+            <div className='grid-item-2'>grid-item-2</div>
+            <div className='grid-item-3'>grid-item-3</div>
+            <div className='grid-item-4'>grid-item-4</div>
+            <div className='grid-item-5'>grid-item-5</div>
+            <div className='grid-item-6'>grid-item-6</div>
           </div>
         </div>
         <div className='footer'>Footer</div>
